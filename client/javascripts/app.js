c02131196254c8f22cf9fbd3a52e5bf1bd9464c6
Axelle Ziegler <Axelle Ziegler>
2017-11-29 09:19:11 +0100

    Liste seulement

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -6,7 +6,6 @@ angular.module('nodeTodo', [])
 
 
   // We'll use that to store data
-  $scope.formData = {};
   $scope.todoData = {};
 
   // This is the http GET action defined in routes/index.js
@@ -17,30 +16,5 @@ angular.module('nodeTodo', [])
     $scope.todoData = data;
   })
 
-  // This code is called when a button is clicked in index.html.
-  $scope.createTodo = () => {
-
-    // This is the http POST action defined in routes/index.js. $scope.formData comes from the web page (index.html) form data
-    $http.post('/api/v1/todos', $scope.formData)
-    .success((data) => {
-
-      // We empty the form
-      $scope.formData = {};
-
-      // We get the new data
-      $scope.todoData = data;
-    })
-  };
-
-  // This code is called when the checkbox is clicked in index.html
-  $scope.deleteTodo = (todoID) => {
-
-    // This is the http DELETE action defined in routes/index.js. todoId comes from index.html
-    $http.delete('/api/v1/todos/' + todoID)
-    .success((data) => {
-
-      // We display the new data
-      $scope.todoData = data;
-    })
-  };
+ 
 });
--- server/routes/index.js
+++ server/routes/index.js
@@ -47,84 +47,8 @@ router.get('/api/v1/todos', (req, res, next) => {
   });
 });
 
-// Handle HTTP Request POST on URL (address) '/api/v1/todos'
-router.post('/api/v1/todos', (request, res, next) => {
-  
-  //We'll store data here
-  const results = [];
-
-  // Get the data sent by the browser (REQUEST)
-  const data = {text: request.body.text, complete: false};
-
-  // Connect to the database
-  pg.connect(connectionString, (err, client, done) => {
-
-    // Handle errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-
-    // Create a query for the database. INSERT means write $1 and $2 get replaced by data.text and data.complete
-    client.query('INSERT INTO ' + tableName + '(text, complete) values($1, $2)',
-    [data.text, data.complete]);
-
-    // Grab the data (as in the first function)
-    const query = client.query('SELECT * FROM ' + tableName + ' ORDER BY id ASC');
-
-    // Store the data in an array
-    query.on('row', (row) => {
-      results.push(row);
-    });
-
-    // When the data is returned
-    query.on('end', () => {
-
-      done();
-
-      // Returns the result to the browser as JSON
-      return res.json(results);
-    });
-  });
-});
 
 
-// Handle HTTP Request DELETE on URL (address) '/api/v1/todos'
-router.delete('/api/v1/todos/:todo_id', (req, res, next) => {
-  
-  // We'll store results here
-  const results = [];
-
-  // Get the data sent by the browser (request)
-  const id = req.params.todo_id;
-
-  // Connect to the database
-  pg.connect(connectionString, (err, client, done) => {
-
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-
-    // Create a request for the SQL server. Delete means... delete ! 
-    client.query('DELETE FROM ' + tableName + ' WHERE id=($1)', [id]);
-
-    // From now on code we've already seen two times
-    var query = client.query('SELECT * FROM ' + tableName + ' ORDER BY id ASC');
-
-    query.on('row', (row) => {
-      results.push(row);
-    });
-
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
-});
 
 
 // Don't worry about that
